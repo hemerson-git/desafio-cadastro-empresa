e3fe706e9b2c6f0233532bb2fedf47f33250b9cc
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-06-11 02:58:08 -0300

    add layout, create home, create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header>
-      <Link to="/cadastrar">
+    <header className="bg-zinc-900 flex justify-end pb-6">
+      <Link
+        to="/cadastrar"
+        className="flex items-center justify-center gap-2 py-2 px-4 rounded-sm bg-purple-500 hover:bg-purple-400 transition-colors"
+      >
         <Plus />
         Criar Cadastro
       </Link>
